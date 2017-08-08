82df2d798840b5dab16c6883b4c65c6d784349e7
drklee3 <drklee3@users.noreply.github.com>
2017-08-07 22:17:15 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# channelplus-download.js
+
+Tampermonkey script to help download images from VLive channel+ posts.
+
+## Usage
+
+1. Install the script.
+2. Open devtools, go to Network and filter by XHR.
+3. Find the /board.xxx/posts url
+4. Open the tampermonkey menu and press download.
---
+++ channelplus-download.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         CHANNEL+ Downloader
+// @version      0.1
+// @description  Downloads CHANNEL+ images
+// @author       Derrick Lee
+// @match        http://api.vfan.vlive.tv/*
+// @grant        GM_registerMenuCommand
+// @grant        GM_download
+// @grant        GM_notification
+// ==/UserScript==
+
+GM_registerMenuCommand('Download images', function() {
+    'use strict';
+    let raw = document.getElementsByTagName('pre')[0].textContent;
+    let data;
+    try {
+        data = JSON.parse(raw);
+    } catch (e) {
+        GM_notification('Failed to parse JSON');
+        return;
+    }
+
+    let posts = data.data;
+    let toDownload = 0;
+    let lastDate;
+    let dateCount = 0;
+
+    for (let i = 0; i < posts.length; i++) {
+        let date = formatDate(new Date(posts[i].created_at));
+        let photos = posts[i].photo;
+
+        for (let key in photos) {
+            toDownload++;
+
+            let fileName;
+            if (lastDate === date) {
+                dateCount++;
+                fileName = date + '_'+ pad(dateCount);
+            } else { // new date
+                dateCount = 0;
+                fileName = date;
+            }
+
+            lastDate = date;
+            GM_download(photos[key].url, fileName);
+        }
+    }
+
+    GM_notification('started ' + toDownload + ' downloads');
+}, 'r');
+
+function formatDate(date) {
+    let yy = date.getFullYear().toString().substr(2);
+    let mm = ('0' + (date.getMonth() + 1).toString()).slice(-2);
+    let dd = ('0' + date.getDate().toString()).slice(-2);
+
+    return yy + mm + dd;
+}
+
+function pad(num) {
+    return ('0' + num).slice(-2);
+}
